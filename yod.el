51720bb71f14b29187a8e51746265b896de067e2
Jonas Bernoulli <jonas@bernoul.li>
2021-05-04 02:38:03 +0200

    Use more conventional summary line

--- yod.el
+++ yod.el
@@ -1,4 +1,4 @@
-;;; yod.el - a yahoo messenger client
+;;; yod.el --- A yahoo messenger client
 
 ;; yod.el is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
